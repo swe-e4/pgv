0038db9b14f26d940e1f00016890ea36492fbc45
TitusKirch <requests@tkirch.dev>
2019-11-13 22:56:03 +0100

    Implement role id to user and rename migration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
+use App\Role;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -23,5 +24,6 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'role_id' => Role::all()->random()->id,
     ];
 });
