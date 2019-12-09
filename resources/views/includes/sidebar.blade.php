30b2f97580da9a527055e0f3d8ebf32ea95f6a56
TitusKirch <requests@tkirch.dev>
2019-12-09 20:32:58 +0100

    feat: add sidebar

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,40 @@
+<ul>
+    <li>
+        <a>
+            <i class="fas fa-thumbtack"></i>
+            <span>Übersicht</span>
+        </a>
+    </li>
+</ul>
+<ul>
+    <li>
+        <a>
+            <i class="fas fa-users"></i>
+            <span>Betreuer</span>
+        </a>
+    </li>
+    <li>
+        <a>
+            <i class="fas fa-project-diagram"></i>
+            <span>Gruppe</span>
+        </a>
+    </li>
+    <li>
+        <a>
+            <i class="fas fa-graduation-cap"></i>
+            <span>Studenten</span>
+        </a>
+    </li>
+    <li>
+        <a>
+            <i class="fas fa-calendar-alt"></i>
+            <span>Termine</span>
+        </a>
+    </li>
+    <li>
+        <a>
+            <i class="fas fa-briefcase"></i>
+            <span>Meilensteine</span>
+        </a>
+    </li>
+</ul>
