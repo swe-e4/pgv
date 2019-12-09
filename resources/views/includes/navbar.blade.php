32ae93a3a21eec5eccda394d98b7940b5ec78621
TitusKirch <requests@tkirch.dev>
2019-12-09 20:32:47 +0100

    feat: add navbar

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav>
+    <div class="brand">
+        <span>{{ config('app.name') }}</span>
+    </div>
+    <div class="menu">
+        <a>
+            <span>tk3208s</span>
+            <i class="fas fa-chevron-down"></i>
+        </a>
+    </div>
+</nav>
