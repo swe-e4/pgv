b88e51ed6aca038993d853708e572e6b90ee7919
TitusKirch <requests@tkirch.dev>
2019-12-09 20:33:14 +0100

    feat: add panel layout

---
+++ resources/views/layouts/panel.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Title -->
+    <title>{{ config('app.name') }} - @yield('title')</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="layout layout-panel">
+    <div id="app">
+        
+        <!-- Navbar -->
+        @include('includes.navbar')
+        
+        <main>
+            <!-- Sidebar -->
+            <div class="sidebar">
+                @include('includes.sidebar')
+            </div>
+
+            <!-- Content -->
+            <div class="content">
+                @yield('content')
+            </div>
+        </main>
+    </div>
+</body>
+</html>
