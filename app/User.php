ce000d60afd4758fb5d39a3b57bf85eea77ccb19
TitusKirch <requests@tkirch.dev>
2019-12-14 03:45:36 +0100

    feat: improve user model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'surname', 'email', 'password', 'api_token', 'role_id',
     ];
 
     /**
@@ -45,5 +45,25 @@ class User extends Authenticatable
     public function role()
     {
         return $this->hasOne('App\Role');
+    }
+
+    /**
+     * groups
+     *
+     * @return void
+     */
+    public function groups()
+    {
+        return $this->hasMany('App\Group');
+    }
+
+    /**
+     * path
+     *
+     * @return void
+     */
+    public function path()
+    {
+        return url('/user/'.$this->id);
     }
 }
