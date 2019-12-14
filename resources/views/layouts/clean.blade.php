9b1069766b3ffe163c58eb775e948d558aadfa5c
TitusKirch <requests@tkirch.dev>
2019-12-14 03:47:00 +0100

    feat: add clean layout

---
+++ resources/views/layouts/clean.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Title -->
+    <title>{{ config('app.name') }} - @yield('title')</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="layout layout-panel">
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
+</html>
