88a94043b79bfb4bb0aa03a877613d561fa6e884
TitusKirch <requests@tkirch.dev>
2019-12-14 03:50:28 +0100

    feat: switch from blade to vue

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav>
+        <div class="brand">
+            <span>NAME</span>
+        </div>
+        <div class="menu">
+            <a>
+                <span>tk3208s</span>
+                <i class="fas fa-chevron-down"></i>
+            </a>
+            <ul>
+                <li class="separator">
+                    <router-link to="/disabled" class="disabled">
+                        <i class="fas fa-cog"></i>
+                        <span>Einstellungen</span>
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/disabled" class="disabled">
+                        <i class="fas fa-sign-out-alt"></i>
+                        <span>Ausloggen</span>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+        <div class="toggle">
+            <i class="fas fa-bars fa-lg"></i>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Sidebar.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="sidebar active">
+        <ul>
+            <li>
+                <router-link to="/">
+                    <i class="fas fa-thumbtack"></i>
+                    <span>Übersicht</span>
+                </router-link>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <router-link to="/adviser/create">
+                    <i class="fas fa-users"></i>
+                    <span>Betreuer</span>
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/disabled" class="disabled">
+                    <i class="fas fa-project-diagram"></i>
+                    <span>Gruppe</span>
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/disabled" class="disabled">
+                    <i class="fas fa-graduation-cap"></i>
+                    <span>Studenten</span>
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/disabled" class="disabled">
+                    <i class="fas fa-calendar-alt"></i>
+                    <span>Termine</span>
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/disabled" class="disabled">
+                    <i class="fas fa-briefcase"></i>
+                    <span>Meilensteine</span>
+                </router-link>
+            </li>
+        </ul>
+        <ul class="only-mobile">
+            <li>
+                <router-link to="/disabled" class="disabled">
+                    <i class="fas fa-cog"></i>
+                    <span>Einstellungen</span>
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/disabled" class="disabled">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span>Ausloggen</span>
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Sidebar"
+    }
+</script>
+
+<style scoped>
+
+</style>
