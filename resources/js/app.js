36528c95f7b066bdeb090427b26b0b33b15b339d
TitusKirch <requests@tkirch.dev>
2019-12-14 23:32:30 +0100

    fix: login bug

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,16 @@
 import Vue from 'vue';
 import router from './router';
 import App from './components/App';
-import vuetron from 'vuetron'
+//import vuetron from 'vuetron'
 
-require('./bootstrap');;
+require('./bootstrap');
 
 //Vue.use(vuetron.VuetronVue);
 
+productionSourceMap: false
 const app = new Vue({
     el: '#app',
+    productionSourceMap: false,
     components: {
         App
     },
