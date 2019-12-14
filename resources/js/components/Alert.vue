4232d7785e7121fc14a332b3cd187d263d5bac8c
TitusKirch <requests@tkirch.dev>
2019-12-14 23:19:21 +0100

    feat: add alert component

---
+++ resources/js/components/Alert.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="alert" :class="type">
+        <h1>{{ title }}</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Alert",
+
+        props: [
+            'type',
+            'title',
+            'message'
+        ],
+    }
+</script>
+
+<style scoped>
+
+</style>
