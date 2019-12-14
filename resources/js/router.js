3140f5aae2bd00ca4858d9df267b1acac0bbcb50
TitusKirch <requests@tkirch.dev>
2019-12-14 23:21:39 +0100

    feat: add missing routes and add titles

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,16 +1,45 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Overview from './views/Overview';
+
+import Adviser from './views/Adviser';
 import AdviserCreate from './views/AdviserCreate';
+import AdviserEdit from './views/AdviserEdit';
+
 import Disabled from './views/Disabled';
+
+import Logout from './actions/Logout';
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
     routes: [
-        { path: '/', component: Overview},
-        { path: '/adviser/create', component: AdviserCreate},
-        { path: '/disabled', component: Disabled}
+        {
+            path: '/', component: Overview,
+            meta: {title: 'Übersicht' }
+        },
+
+        {
+            path: '/adviser', component: Adviser,
+            meta: {title: 'Betreuer' }
+        },
+        {
+            path: '/adviser/create', component: AdviserCreate,
+            meta: {title: 'Betreuer hinzufügen' }
+        },
+        {
+            path: '/adviser/:id/edit', component: AdviserEdit,
+            meta: {title: 'Betreuer bearbeiten' }
+        },
+
+        {
+            path: '/disabled', component: Disabled,
+            meta: {title: 'Deaktiviert' }
+        },
+
+        {
+            path: '/logout', component: Logout
+        }
     ],
     mode: 'history'
 });
