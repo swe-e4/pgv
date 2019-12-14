88313ce6b86dd40efe1b5ff03fc3cc177a70a014
TitusKirch <requests@tkirch.dev>
2019-12-14 03:51:14 +0100

    feat: add disabled view

---
+++ resources/js/views/Disabled.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="alert warning">
+            <h1>Warnung</h1>
+            <p>Diese Funktion ist derzeit deaktiviert.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Disabled"
+    }
+</script>
+
+<style scoped>
+
+</style>
