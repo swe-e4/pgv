8af87443a3e9058a6a606dcbc885fc19377ba75a
TitusKirch <requests@tkirch.dev>
2019-12-14 23:17:05 +0100

    feat: remove laravel routes

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,4 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::get('/login', function(){
-    return view('pages.login');
-})->name('login');
-
-Route::get('/logout', function(){
-    Auth::logout();
-    return redirect()->route('login');
-})->name('logout');
-
 Route::get('/{any}', 'AppController@index')->where('any', '.*');
