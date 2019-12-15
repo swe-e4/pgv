6079e59bbf605f4736aa972fc01803cb59791327
TitusKirch <requests@tkirch.dev>
2019-12-15 03:14:06 +0100

    fix: use lastname for surname

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'surname' => $faker->name,
+        'surname' => $faker->lastName,
         'first_name' => $faker->firstName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
