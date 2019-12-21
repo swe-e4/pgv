5bb5339ee9fdddf86e9b150c74205b090fdec6e8
TitusKirch <requests@tkirch.dev>
2019-12-21 14:11:11 +0100

    feat: disable password creation

--- app/Http/Controllers/AdviserController.php
+++ app/Http/Controllers/AdviserController.php
@@ -34,7 +34,7 @@ class AdviserController extends Controller
     private function requiredData()
     {
         return [
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'nope', //'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'api_token' => Str::random(60),
             'role_id' => 2,
         ];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'surname' => $faker->lastName,
         'first_name' => $faker->firstName,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => 'nope', //'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'api_token' => Str::random(60),
         'role_id' => 2,
