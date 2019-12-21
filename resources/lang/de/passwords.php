f7022876683af49f568520de92c3d98b1f4c4587
TitusKirch <requests@tkirch.dev>
2019-12-21 12:48:18 +0100

    feat: add german translation

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ihr Passwort wurde zurückgesetzt!',
+    'sent' => 'Du hast eine Email erhalten, mit der du dein Passwort zurücksetzten kannst!',
+    'token' => 'Dieser Passwort-zurüksetzten-Link ist nicht gültig.',
+    'user' => "Es wurde kein User mit dieser Email-Adresse gefunden.",
+    'throttled' => 'Bitte warte einen Augenblick.',
+
+];
