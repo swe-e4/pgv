6ddcc2e8897d34aca09a24d15c92ffcc71b2a58a
TitusKirch <requests@tkirch.dev>
2019-12-21 18:08:28 +0100

    feat: create empty appointment resource and policy

---
+++ app/Policies/AppointmentPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Appointment;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class AppointmentPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any appointments.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the appointment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Appointment  $appointment
+     * @return mixed
+     */
+    public function view(User $user, Appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create appointments.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the appointment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Appointment  $appointment
+     * @return mixed
+     */
+    public function update(User $user, Appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the appointment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Appointment  $appointment
+     * @return mixed
+     */
+    public function delete(User $user, Appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the appointment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Appointment  $appointment
+     * @return mixed
+     */
+    public function restore(User $user, Appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the appointment.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Appointment  $appointment
+     * @return mixed
+     */
+    public function forceDelete(User $user, Appointment $appointment)
+    {
+        //
+    }
+}
