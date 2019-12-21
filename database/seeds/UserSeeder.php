542cde250bf499b37feaa6afc139b027596c179d
TitusKirch <requests@tkirch.dev>
2019-12-21 18:09:15 +0100

    feat: rename default user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,8 +15,8 @@ class UserSeeder extends Seeder
     {
         //create a lecturer 
         User::create([
-            'surname' => "Kirch",
-            'first_name' => "Titus",
+            'surname' => "Mustermann",
+            'first_name' => "Max",
             'email' => "mail@example.com",
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
