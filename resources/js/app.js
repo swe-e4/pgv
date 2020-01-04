c82ca83a6102f261b5290e39caf79766b6683430
TitusKirch <requests@tkirch.dev>
2020-01-04 19:37:13 +0100

    feat: add v-calendar

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,10 @@ import router from './router';
 import App from './components/App';
 
 import Datetime from 'vue-datetime';
+import VCalendar from 'v-calendar';
 //import vuetron from 'vuetron'
 Vue.use(Datetime)
+Vue.use(VCalendar)
 require('./bootstrap');
 
 //Vue.use(vuetron.VuetronVue);
