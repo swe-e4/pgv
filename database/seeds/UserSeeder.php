20a11086b12f234d3214e3f64a1af385395e058c
TitusKirch <requests@tkirch.dev>
2020-01-14 20:04:08 +0100

    feat: add example adviser

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -24,6 +24,17 @@ class UserSeeder extends Seeder
             'role_id' => 1,
         ]);
 
+        //create a adviser 
+        User::create([
+            'surname' => "Musterfrau",
+            'first_name' => "Anna",
+            'email' => "anna@example.com",
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'api_token' => "GUUYBjQZLqEEHNbR3PE4yk86460kYfe9VO7NBF8t0l21nTxKDtXCcHyds9GK",
+            'role_id' => 2,
+        ]);
+
         factory(\App\User::class, 4)->create();
     }
 }
